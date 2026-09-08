55237f220a57b43f3491c364202fddadaa3ba94f
memahin <rafiul.mahin1@gmail.com>
2026-09-08 17:41:05 +0600

    react practice

---
+++ src/TestComponent.jsx
@@ -0,0 +1,34 @@
+// Component
+
+// const TestComponent = () => {
+//     return <h1>Md. Rafiul Islam</h1>;
+// }
+
+// export default TestComponent;
+
+// export default function TestComponent() {
+//     return <h1>Md. Rafiul Islam Mahin</h1>
+// }
+
+// export const TestComponent = () => {
+//     return <h1>Md. Rafiul Islam</h1>;
+// }
+const name = "Mahin";
+export default function TestComponent() {
+    return (
+    <>
+        <div>
+            <h1>Hello, World!</h1>
+            <p>
+                My name is {name}. I am a passionate about creating scalable mobile applications that deliver exceptional user experiences across platforms. I enjoy learning new technologies and continuously improving my skills in the field of Computer Science Engineer with a solid foundation in mobile application design and development.    
+            </p>
+        </div>
+        <div>
+            <h1>Skills & Expertise</h1>
+            <p>
+                I have a strong foundation in mobile application development, with expertise in React Native, Flutter, and Kotlin. I am proficient in building cross-platform applications that are responsive, user-friendly, and optimized for performance. I have experience working with RESTful APIs, integrating third-party libraries, and implementing state management solutions such as Redux and MobX. Additionally, I am skilled in UI/UX design principles and have a keen eye for creating visually appealing interfaces that enhance the overall user experience.
+            </p>
+        </div>
+    </>
+    )
+}
